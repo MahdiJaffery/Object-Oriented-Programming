577c120fd436691eca89937c548447d5109f3b43
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-08-02 11:39:33 +0500

    Operator Overloading

---
+++ Classes-5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+// Lecture 012, 7th March 2023
+
+void AnotherClass::operator=(const AnotherClass &obj)
+{
+    if (arr != nullptr)
+    {
+        delete[] arr;
+        arr = 0;
+    }
+    size = obj.size;
+    arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = obj.arr[i];
+    }
+    // All Operators except some e.g.(.) can be overloaded
+}
+
+int main()
+{
+    AnotherClass obj1;
+    AnotherClass obj2 = obj1;
+    AnotherClass obj3(10);
+
+    obj1 = obj3;
+
+    return 0;
+}
